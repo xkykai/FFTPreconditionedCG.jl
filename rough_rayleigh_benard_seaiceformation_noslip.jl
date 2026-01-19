7af24e378529119b3d4fbe01dc690c76eafd778a
Xin Kai Lee <xinkai@mit.edu>
2026-01-19 13:44:50 -0500

    update output filename and adjust simulation parameters for bounded and unbounded cases

--- rough_rayleigh_benard_seaiceformation_noslip.jl
+++ rough_rayleigh_benard_seaiceformation_noslip.jl
@@ -108,7 +108,7 @@ elseif solver_type == "CG"
     pressure_solver_str = solver_type
 end
 
-filename = "rough_RB_seaiceformation_noslip_Ra_$(Ra)_Pr_$(Pr)_Nr_$(Nr)_Lx_$(Lx)_Lz_$(Lz)_Nx_$(Nx)_Nz_$(Nz)_$(pressure_solver_str)"
+filename = "rough_RB_seaiceformation_noslip_Ra_$(Ra)_Pr_$(Pr)_Nr_$(Nr)_Lx_$(Lx)_Lz_$(Lz)_Nx_$(Nx)_Nz_$(Nz)_$(pressure_solver_str)_2"
 
 FILE_DIR = "./Data/$(filename)"
 mkpath(FILE_DIR)
@@ -155,7 +155,7 @@ c₁(x, z) = 1
 
 set!(model, T=Tᵢ, c=c₁, S=Sᵢ)
 
-stop_time = 20000
+stop_time = 5000
 advective_Δt = (Lz / Nz) / Δb
 diffusive_Δt = min((Lx / Nx)^2, Lz/Nz^2) / max(ν, κ)
 Δt = min(advective_Δt, diffusive_Δt) / 10
@@ -224,19 +224,19 @@ KEbar = Average(0.5 * (u^2 + w^2), dims=(1, 2, 3))
 
 simulation.output_writers[:jld2] = JLD2Writer(model, (; u, w, T, S, c, b, d, pNHS = model.pressures.pNHS);
                                               filename = joinpath(FILE_DIR, "instantaneous_fields.jld2"),
-                                              schedule = TimeInterval(100),
+                                              schedule = TimeInterval(50),
                                               with_halos = true,
                                               overwrite_existing = true)
 
 simulation.output_writers[:averaged] = JLD2Writer(model, (; T = Tbar, S = Sbar, b = bbar, Nu);
                                               filename = joinpath(FILE_DIR, "averaged_fields.jld2"),
-                                              schedule = AveragedTimeInterval(1000, window=1000),
+                                              schedule = AveragedTimeInterval(2500, window=2500),
                                               with_halos = false,
                                               overwrite_existing = true)
 
 simulation.output_writers[:KE] = JLD2Writer(model, (; KE = KEbar);
                                               filename = joinpath(FILE_DIR, "KE_fields.jld2"),
-                                              schedule = AveragedTimeInterval(10, window=10),
+                                              schedule = AveragedTimeInterval(50, window=50),
                                               with_halos = false,
                                               overwrite_existing = true)
 
